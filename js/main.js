bdd0fd183ab019b3f31318f63de91f69378d5e91
r4u2o2 <pome.ku46@gmail.com>
2021-07-15 21:52:41 +0900

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    // スラドショー
+    $('#slideContents').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 500,
+        dots: true,
+        pauseOfHover: false,
+        pauseOnHover: false,
+        fade: true,
+    });
+
+
+});
