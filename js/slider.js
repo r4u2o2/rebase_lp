937f62936537093072d1fc425c0f6a6fe32362a9
r4u2o2 <pome.ku46@gmail.com>
2021-07-16 12:24:04 +0900

    update

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+$('.slider').slick({
+
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
