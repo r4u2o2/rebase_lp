05e0b28654d971c0322fa88219768bd5b57ce60b
r4u2o2 <pome.ku46@gmail.com>
2021-07-16 16:43:48 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,51 @@
 $(function(){
-    // スラドショー
-    $('#slideContents').slick({
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 500,
-        dots: true,
-        pauseOfHover: false,
-        pauseOnHover: false,
-        fade: true,
+    
+    // ------
+    // Rental Price tab
+    // -----
+
+    $('#tab01').on('click', function(){
+        console.log('34クリック');
+        $('.tab-line').css('background', '#EBEBEB');
+        $('#tab01 > .tab-line').css('background', '#B3EFB3');
+        $('.tab-box > li').css('font-size', '14px');
+        $('.tab01').css('font-size', '33px');
+        $('.slider').css('display', 'none');
+        $('.people34').css('display', 'block');
+    });
+    $('#tab02').on('click', function(){
+        console.log('12クリック');
+        $('.tab-line').css('background', '#EBEBEB');
+        $('#tab02 > .tab-line').css('background', '#B3EFB3');
+        $('.tab-box > li').css('font-size', '14px');
+        $('.tab02').css('font-size', '33px');
+        $('.slider').css('display', 'none');
+        $('.people12').css('display', 'block');
+    });
+    $('#tab03').on('click', function(){
+        console.log('ソロクリック');
+        $('.tab-line').css('background', '#EBEBEB');
+        $('#tab03 > .tab-line').css('background', '#B3EFB3');
+        $('.tab-box > li').css('font-size', '14px');
+        $('.tab03').css('font-size', '30px');
+        $('.slider').css('display', 'none');
+        $('.soro').css('display', 'block');
     });
 
+    // ------
+    // Q&A
+    // -----
 
+    //アコーディオンをクリックした時の動作
+    $('.qa-title').on('click', function() {//タイトル要素をクリックしたら
+        console.log('QAクリック');
+    var findElm = $(this).next('.answer');//直後のアコーディオンを行うエリアを取得し
+    $(findElm).slideToggle();//アコーディオンの上下動作
+      
+    if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+      $(this).removeClass('close');//クラス名を除去し
+    }else{//それ以外は
+      $(this).addClass('close');//クラス名closeを付与
+    }
+  });
 });
--- js/slider.js
+++ js/slider.js
@@ -6,11 +6,14 @@ $('.slider').slick({
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 1,
+    lazyLoad: 'progressive',
+    // mobileFirst: true,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
     responsive: [
       {
         breakpoint: 768,
         settings: {
-          arrows: true,
           centerMode: true,
           centerPadding: '20px',
           slidesToShow: 1
@@ -19,7 +22,6 @@ $('.slider').slick({
       {
         breakpoint: 480,
         settings: {
-          arrows: true,
           centerMode: true,
           centerPadding: '20px',
           slidesToShow: 1
